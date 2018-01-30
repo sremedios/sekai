750ab221ef3bbd496cc74278b9224116e90d05b8
Steven Sheffey <stevensheffey4@gmail.com>
2018-01-30 13:04:46 -0600

    Added sends/receives and fixed compile errors

--- src/entity.rs
+++ src/entity.rs
@@ -1,5 +1,12 @@
+use world::World;
+
 /// Defines an entity within a world
-pub trait Entity {
+pub trait Entity<T> {
     /// Updates the entity based on the world around it
-    pub fn update(&mut self, world: World);
+    fn update(&mut self, world: &World);
+    /// Sends a message to every entity in the world
+    /// TODO: add some kind of filter
+    fn send_all(&self, message: T, world: &World);
+    /// Receives a message
+    fn receive(&mut self, message: T);
 }
--- src/world.rs
+++ src/world.rs
@@ -1,7 +1,7 @@
 /// Defines an object representing a world
 pub trait World {
     /// Updates the world 1 tick
-    pub fn update(&mut self);
+    fn update(&mut self);
     /// Gets the number of entities in the world
-    pub fn num_entities(&self) -> usize;
+    fn num_entities(&self) -> usize;
 }
