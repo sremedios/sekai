9efa315c60ae42f8b760c279d58a625e60e31fb1
Steven Sheffey <stevensheffey4@gmail.com>
2018-01-30 13:30:17 -0600

    Added some more message stuff, and an example for the docs

--- src/entity.rs
+++ src/entity.rs
@@ -1,12 +1,42 @@
 use world::World;
 
 /// Defines an entity within a world
-pub trait Entity<T> {
+/// # Arguments
+/// * `M` - The type to use for messages between entities
+/// # Example
+/// ```
+/// struct Cat {
+///     hunger: i64,
+/// }
+/// impl Entity<String> for Cat {
+///     /// Every tick, the cat gets more hungry
+///     /// If the cat gets too hungry, it meows
+///     fn update(&mut self, world: &World) {
+///         self.hunger += 1;
+///         if self.hunger >= 100 {
+///             self.send_all("MEOW".into());
+///         }
+///     }
+///     /// Cats send messages to stdout
+///     fn send_all(&self, message: String, world: &World) {
+///         println!("{}", message);
+///     }
+///     /// Cats ignore any incoming messages
+///     fn receive(&mut  self, message: T) {}
+/// }
+/// ```
+pub trait Entity<M> {
     /// Updates the entity based on the world around it
-    fn update(&mut self, world: &World);
+    /// # Arguments
+    /// * `world` - the world the entity exists in
+    fn update(&mut self, world: &World<M>);
     /// Sends a message to every entity in the world
-    /// TODO: add some kind of filter
-    fn send_all(&self, message: T, world: &World);
-    /// Receives a message
-    fn receive(&mut self, message: T);
+    /// # Arguments
+    /// * `message` - The message being sent to all other entities
+    /// * `world` - The world the entity exists in
+    fn send_all(&self, message: M, world: &World<M>);
+    /// Handler for receiving a message
+    /// # Arguments
+    /// * `message` - The message to receive
+    fn receive(&mut self, message: M);
 }
--- src/world.rs
+++ src/world.rs
@@ -1,7 +1,9 @@
 /// Defines an object representing a world
-pub trait World {
+pub trait World<M> {
     /// Updates the world 1 tick
     fn update(&mut self);
     /// Gets the number of entities in the world
     fn num_entities(&self) -> usize;
+    /// Handles a message sent by an entity
+    fn receive_message(&mut self, message: M);
 }
