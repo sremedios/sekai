ba4657d453ca042fd134ec73daa169c1b504d340
sremedios <sam.remedios@gmail.com>
2018-02-01 11:47:42 -0600

    started examples dir

---
+++ examples/firefly_synchronization.rs
@@ -0,0 +1,14 @@
+/*
+ * Synchronize flashing of a firefly swarm
+ *
+ */
+extern crate sekai;
+use world::World;
+use entity::Entity;
+
+// let test_world: World = World::new();
+
+#[test]
+fn test_examples() {
+    assert_eq!(2, 2);
+}
