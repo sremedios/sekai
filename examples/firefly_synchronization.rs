db0a7de6384491110086f373d9787a5107475aa8
sremedios <sam.remedios@gmail.com>
2018-02-01 17:42:47 -0600

    fixed a bunch of syntax errors, added functionality

--- examples/firefly_synchronization.rs
+++ examples/firefly_synchronization.rs
@@ -3,18 +3,31 @@
  *
  */
 extern crate sekai;
-use world::World;
-use entity::Entity;
+use sekai::world::World;
+use sekai::entity::Entity;
 
 struct FireflyWorld {
+    firefly_swarm: Vec<Firefly>,
 }
-impl World for FireflyWorld {
+impl World<(u8, u8, u8)> for FireflyWorld {
     // todo: figure out if a firefly can see another firefly
     fn update(&mut self) {
         // Iterate through all fireflies in a specifc range,
-        // average color 
-        for firefly in firefly_swarm {
-            
+        // average color
+        for firefly in self.firefly_swarm {
+            println!("Updating firefly.");
+        }
+    }
+
+    // returns the number of fireflies in the swarm
+    fn num_entities(&self) -> usize {
+        self.firefly_swarm.len()
+    }
+
+    // calls receive message on every firefly
+    fn receive_message(&mut self, message: (u8, u8, u8)) {
+        for firefly in self.firefly_swarm {
+            firefly.receive_message(message);
         }
     }
 }
@@ -30,29 +43,31 @@ struct Firefly {
 /// Fireflies communicate with lights, represented in the
 /// tuple (RGB)
 impl Entity<(u8, u8, u8)> for Firefly {
-    // todo: receive message, send message, 
-    fn update(&mut self, world: &World) {
-        self.cur_flash_cooldown -= flash_rate;
+    // todo: receive message, send message,
+    fn update(&mut self, world: &sekai::world::World<(u8, u8, u8)>) {
+        self.cur_flash_cooldown -= self.flash_rate;
         if self.cur_flash_cooldown == 0 {
             self.cur_flash_cooldown = self.flash_cooldown;
         }
     }
-    fn receive_message(&mut self, message: <(u8,u8,u8)>){
+    fn receive_message(&mut self, message: (u8, u8, u8)) {
         // If a firefly sees some color, it must by some logic
         // update its own flash_rate and color
-        
+
         // Placeholder logic for now
         // If all message lights that were received were averaged:
         self.color = message;
         // If received, reset cur_flash_cooldown
         self.cur_flash_cooldown = self.flash_cooldown;
         // how to update flash rate?  should this even be parameterized?
-        
-        
+
+
     }
 }
 
-// let test_world: World = World::new();
+fn main() {
+    println!("This is the main function");
+}
 
 #[test]
 fn test_examples() {
