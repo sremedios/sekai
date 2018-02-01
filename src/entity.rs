abb22cc1efc4fbd09a76980657741301729f3674
starfys <starfys@users.noreply.github.com>
2018-02-01 11:46:31 -0600

    [HOLD] More trait definitions (#7)
    * Added some more to the world trait
    
    * Added sends/receives and fixed compile errors
    
    * Added some more message stuff, and an example for the docs
    
    * Slight changes
    
    * Fixed a doc comment
    
    * Documented a function and made functions public

--- src/entity.rs
+++ src/entity.rs
@@ -1,5 +1,41 @@
+use world::World;
+
 /// Defines an entity within a world
-pub trait Entity {
+/// # Arguments
+/// * `M` - The type to use for messages between entities
+/// # Example
+/// ```rust
+/// struct Cat {
+///     hunger: i64,
+/// }
+/// impl Entity<String> for Cat {
+///     /// Every tick, the cat gets more hungry
+///     /// If the cat gets too hungry, it meows
+///     fn update(&mut self, world: &World) {
+///         self.hunger += 1;
+///         if self.hunger >= 100 {
+///             self.send_message("MEOW".into());
+///         }
+///     }
+///     /// Cats ignore any incoming messages
+///     fn receive_message(&mut  self, message: T) {}
+/// }
+/// ```
+pub trait Entity<M> {
     /// Updates the entity based on the world around it
-    pub fn update(&mut self, world: World);
+    /// # Arguments
+    /// * `world` - the world the entity exists in
+    pub fn update(&mut self, world: &World<M>);
+    /// Sends a message to the world. The world will choose which entities to
+    /// forward the message to
+    /// # Arguments
+    /// * `message` - The message being sent to all other entities
+    /// * `world` - The world the entity exists in
+    pub fn send_message(&self, message: M, world: &mut World<M>) {
+        world.receive_message(message)
+    }
+    /// Handler for receiving a message
+    /// # Arguments
+    /// * `message` - The message to receive
+    pub fn receive_message(&mut self, message: M);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,4 @@ pub mod entity;
 pub mod world;
 
 #[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+mod tests {}
